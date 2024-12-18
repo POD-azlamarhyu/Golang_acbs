ce177558ec5fc6581f1f426abffc239b7949fe23
azla mahryu <sgommccisbad72145@gmail.com>
2024-12-19 02:57:40 +0900

    update template mod gofile

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 )
 
+func createArray(n int) []int{
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	line := strings.Fields(sc.Text())
+
+	a := make([]int, n)
+	for i := range line {
+		a[i], _ = strconv.Atoi(line[i])
+	}
+
+	return a
+}
 
 func main(){
 	var a int
